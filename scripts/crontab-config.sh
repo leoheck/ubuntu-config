45c725d670e2f3886cec3b93e691b54cd529ff68
Leandro Heck <leoheck@gmail.com>
2016-05-29 22:02:54 -0300

    Updating and testing configure script

--- scripts/crontab-config.sh
+++ scripts/crontab-config.sh
@@ -12,7 +12,7 @@ crontab << EOF
 # * * * * * touch /tmp/gaph-upgrade-\$(date +\%Y-\%m-\%d:\%H:\%M)
 
 # Upgrade host from github (using 1x/min for tests only)
-* * * * * /usr/bin/upgrade-gaph-host && /tmp/upgrade-gaph-host-done
+* * * * * root /usr/bin/upgrade-gaph-host && /tmp/upgrade-gaph-host-done
 
 # Update /etc/hosts file (4x/day)
 #30 7,12,18,23 * * * /soft64/admin/scripts/update-hosts.sh
