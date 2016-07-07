8b00debd19b4aa1a33bbb230eaa809b6d77c42d8
Leandro Heck <leoheck@gmail.com>
2016-07-07 13:55:08 -0300

    Updating crontab

--- scripts/crontab-config.sh
+++ scripts/crontab-config.sh
@@ -48,11 +48,11 @@ install_cmd()
 	#===========================
 	read -r -d '' CRONCONF <<-EOM
 
-	# [WARNING] THE ROOT Crontab is updated automatically. 
+	# [WARNING] THE ROOT Crontab is updated automatically.
 	# Put your cronjobs in the **user** crontab.
 
 	# TESTING (1x/day)
-	0 0 * * * root /usr/bin/upgrade-gaph-host
+	0 0 * * * root /usr/bin/upgrade-gaph-host 2>&1 /var/log/gaph/upgrade-gaph-host
 
 	# Keep /etc/salt/minion updated and running (1x/day)
 	0 2 * * * echo "$(hostname)" > /etc/salt/minion_id; sed -i "s/^[#]*master:.*/master: rodos/g" /etc/salt/minion; service salt-minion restart
