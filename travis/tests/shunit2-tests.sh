07d8067837f307036f4bc945dbc965caae67a7f0
Leandro Heck <leoheck@gmail.com>
2017-01-25 17:43:58 -0200

    Update shunit2-tests.sh

--- travis/tests/shunit2-tests.sh
+++ travis/tests/shunit2-tests.sh
@@ -7,10 +7,16 @@ testEquality() {
 	assertEquals 1 1
 }
 
-testPartyLikeItIs1999() {
+# testPartyLikeItIs1999() {
+# 	year=$(date '+%Y')
+# 	assertEquals "It's not 1999 :-(" '1999' "${year}"
+# }
+
+testPartyLikeItIs2017() {
 	year=$(date '+%Y')
-	assertEquals "It's not 1999 :-(" '1999' "${year}"
+	assertEquals "It's not 2017 :-(" '2017' "${year}"
 }
+
 
 # load shunit2
 . shunit2
