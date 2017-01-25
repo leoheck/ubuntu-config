8c52af7a7437293d584658daa8e6ab5ceb21de29
Leandro Heck <leoheck@gmail.com>
2017-01-25 00:45:01 -0200

    Testing

--- travis/lxd/script.sh
+++ travis/lxd/script.sh
@@ -2,5 +2,9 @@
 
 set -e -x 
 
-ssh root@$(cat FEDORA_IP.txt) -o UserKnownHostsFile=ssh_keys/known_hosts -i ssh_keys/insecure dnf -y upgrade 
-ssh root@$(cat FEDORA_IP.txt) -o UserKnownHostsFile=ssh_keys/known_hosts -i ssh_keys/insecure dnf -y install htop tmux  
+# ssh root@$(cat FEDORA_IP.txt) -o UserKnownHostsFile=ssh_keys/known_hosts -i ssh_keys/insecure dnf -y upgrade 
+# ssh root@$(cat FEDORA_IP.txt) -o UserKnownHostsFile=ssh_keys/known_hosts -i ssh_keys/insecure dnf -y install htop tmux  
+
+echo
+echo "SCRIPT RUNNING <<<<====================="
+echo
