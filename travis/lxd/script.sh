2c51101bc2e3b4cc58dd72f23c839f47024ab2b8
Leandro Heck <leoheck@gmail.com>
2017-01-25 00:51:20 -0200

    Testing

--- travis/lxd/script.sh
+++ travis/lxd/script.sh
@@ -6,5 +6,17 @@ set -e -x
 # ssh root@$(cat FEDORA_IP.txt) -o UserKnownHostsFile=ssh_keys/known_hosts -i ssh_keys/insecure dnf -y install htop tmux  
 
 echo
-echo "SCRIPT RUNNING <<<<====================="
+echo "LHECK TESTS <<<<====================="
+echo
+
+lxc exec fedora -- bash -c 'hostname'
+lxc exec fedora -- bash -c 'pwd'
+lxc exec fedora -- bash -c 'ls -lsa'
+lxc exec fedora -- bash -c 'uname -a'
+lxc exec fedora -- bash -c 'lsb_release'
+lxc exec fedora -- bash -c 'cat /etc/hosts'
+lxc exec fedora -- bash -c 'uptime'
+
+echo
+echo "TESTS DONE <<<<====================="
 echo
